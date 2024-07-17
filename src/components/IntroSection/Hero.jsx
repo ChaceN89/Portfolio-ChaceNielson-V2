34f59d17828d13e6fb510296062d4d7ac578d5a5
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-07-16 18:33:56 -0600

    got hero working - need to edit CSS still

--- src/components/IntroSection/Hero.jsx
+++ src/components/IntroSection/Hero.jsx
@@ -6,6 +6,30 @@ import { Link as ScrollLink } from 'react-scroll';
 import { globals } from '../../googleAnalytics/globals';
 
 function Hero() {
+  const textVariants = {
+    initial: {
+      x: -500,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.1,
+      },
+    },
+    scrollButton: {
+      opacity: 0,
+      y: 10,
+      transition: {
+        duration: 2,
+        repeat: Infinity,
+      },
+    },
+  };
+
+
   const sliderVariants = {
     initial: {
       x: 0,
@@ -42,7 +66,7 @@ function Hero() {
         initial="initial"
         animate="animate"
       >
-        &nbsp;&nbsp; Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate.
+        &nbsp;&nbsp; Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate. Code. Create. Innovate.
       </motion.div>
 
       <div className="w-full h-full flex justify-center items-end absolute inset-0">
@@ -68,21 +92,53 @@ function Hero() {
       </div>
 
 
-      <div className="grid grid-cols-2 h-full">
-        <div className="flex flex-col justify-center gap-10 p-4 ">
-          <h3>Hi my name is</h3>
-          <h1>CHACE NIELSON</h1>
-          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquam exercitationem commodi excepturi neque minus.</h3>
-          <div className="buttons flex justify-center md:justify-start">
-            <button className="p-5 border border-white rounded-lg bg-transparent text-white mr-5 cursor-pointer font-light">My Projects</button>
-            <button className="p-5 border border-white rounded-lg bg-transparent text-white cursor-pointer font-light">Let's work Together</button>
-          </div>
-        </div>
-        <div className="flex items-end h-full">
-          <ImageComponent src={`${process.env.PUBLIC_URL}/logos/chace2.png`} alt="Hero" className="w-full max-h-3/4 object-contain" />
-        </div>
-      </div>
+      <div className="wrapper">
 
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>CHACE NIELSON</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Web developer and UI designer
+          </motion.h1>
+
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>
+              See the Latest Works
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+
+          <motion.div variants={textVariants}  className="buttons flex justify-center md:justify-start">
+            <motion.button 
+              variants={textVariants}  
+              className="p-5 border border-white rounded-lg bg-transparent text-white mr-5 cursor-pointer font-light"
+            >
+                My Projects
+            </motion.button>
+            <motion.button 
+              variants={textVariants}  
+              className="p-5 border border-white rounded-lg bg-transparent text-white cursor-pointer font-light"
+            >
+              Let's work Together
+            </motion.button>
+          </motion.div>
+
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png"
+            alt=""
+          />
+        </motion.div>
+
+      </div>
+      <div className="imageContainer">
+        <ImageComponent src={`${process.env.PUBLIC_URL}/logos/chace2.png`} alt="Hero" className="w-full max-h-3/4 object-contain" />
+      </div>
 
 
 
@@ -91,3 +147,16 @@ function Hero() {
 }
 
 export default Hero;
+
+
+
+
+<div className="flex flex-col justify-center gap-10 p-4 ">
+  <h3>Hi my name is</h3>
+  <h1>CHACE NIELSON</h1>
+  <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquam exercitationem commodi excepturi neque minus.</h3>
+  <div className="buttons flex justify-center md:justify-start">
+    <button className="p-5 border border-white rounded-lg bg-transparent text-white mr-5 cursor-pointer font-light">My Projects</button>
+    <button className="p-5 border border-white rounded-lg bg-transparent text-white cursor-pointer font-light">Let's work Together</button>
+  </div>
+  </div>
