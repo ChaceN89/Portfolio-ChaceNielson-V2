cbeb8b679e825a53ce4b35f624cc7a082300317e
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-07-18 22:10:42 -0600

    added a couple of testing file and images

---
+++ src/testingComponents/BlurTests.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function BlurTests() {
+  return (
+    <div
+      className="section-wrapper-height overflow-hidden relative flex items-center justify-center px-6"
+    >
+      <img 
+        src={process.env.PUBLIC_URL + '/jpg-portraits/japan-portrait-trim.jpg'} 
+        alt="im not loaded yet" 
+      />
+      
+
+    </div>
+  )
+}
+
+export default BlurTests
