0386a56b4e54500bc498383efe3f6e89a5fb98ff
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-07-26 19:16:19 -0600

    added hash borwser feaure

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,6 @@
-
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { animateScroll as scroll, scroller } from 'react-scroll';
 
 import Hero from '../homeSections/hero/Hero';
 import CallToAction from '../homeSections/callToAction/CallToAction';
@@ -9,17 +11,34 @@ import ContactForm from '../homeSections/contactForm/ContactForm';
 
 import ReturnToTop from '../components/uiElements/ReturnToTop';
 
+import { globals } from '../data/globals';
 
 const HomePage = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const sectionId = location.hash.replace('#', '');
+      scroller.scrollTo(sectionId, {
+        offset: globals.ScrollLink.offset +15,  // Adjust this offset based on your fixed navbar height
+        duration: globals.ScrollLink.duration,
+        spy:true,
+        smooth:true
+      });
+    } else {
+      scroll.scrollToTop();
+    }
+  }, [location]);
+
   return (
     <>
       <div id='Home'>
-        <Hero/>
+        <Hero />
         <CallToAction />
       </div>
       <div id='AboutMe'>
         <AboutMe />
-        <JobExperience/>
+        <JobExperience />
       </div>
       <div id='Projects'>
         <ProjectSection />
@@ -27,10 +46,9 @@ const HomePage = () => {
       <div id='ContactMe'>
         <ContactForm />
       </div>
-
       <ReturnToTop />
     </>
   );
 };
 
-export default HomePage;  
+export default HomePage;
