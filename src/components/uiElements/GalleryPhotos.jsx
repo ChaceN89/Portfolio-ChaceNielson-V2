0a8fa90abb2d6cf77516da8dfd758db97776bf6a
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-07-28 10:28:53 -0600

    added check in photos for small screens

--- src/components/uiElements/GalleryPhotos.jsx
+++ src/components/uiElements/GalleryPhotos.jsx
@@ -5,6 +5,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import ImageComponent from '../../wrappers/ImageComponent';
 import { galleryPhotos } from '../../data/pageData/galleryData';
 import LazyLoad from 'react-lazy-load';
+import { useMediaQuery } from 'react-responsive';
 
 Modal.setAppElement('#root'); // This is to avoid screen reader issues
 
@@ -12,6 +13,7 @@ export default function GalleryPhotos() {
   const [activePhotoIndex, setActivePhotoIndex] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [direction, setDirection] = useState(0);
+  const isLargeScreen = useMediaQuery({ query: '(min-width: 640px)' }); // Tailwind's sm breakpoint is 640px
 
   useEffect(() => {
     const handleKeyDown = (event) => {
@@ -34,8 +36,10 @@ export default function GalleryPhotos() {
   }, [isModalOpen]);
 
   const openModal = (index) => {
-    setActivePhotoIndex(index);
-    setIsModalOpen(true);
+    if (isLargeScreen) {
+      setActivePhotoIndex(index);
+      setIsModalOpen(true);
+    }
   };
 
   const closeModal = () => {
@@ -86,7 +90,7 @@ export default function GalleryPhotos() {
                 <ImageComponent
                   src={`${process.env.PUBLIC_URL}/gallery-photos/${item.photo}`}
                   alt={`Photo ${index + 1}`}
-                  className='object-cover w-full h-full rounded-sm cursor-pointer'
+                  className='object-cover w-full h-full rounded-sm sm:cursor-pointer'
                   blurHash={item.blurhash}
                 />
               </div>
@@ -95,55 +99,57 @@ export default function GalleryPhotos() {
         </div>
       ))}
 
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        contentLabel="Photo Modal"
-        className="flex items-center justify-center outline-none"
-        overlayClassName="fixed inset-0 bg-black bg-opacity-50 z-30 flex items-center justify-center"
-      >
-        <div className='relative z-35 h-120 min-w-[34rem] w-full bg-secondary p-10 rounded'>
-          <button
-            onClick={closeModal}
-            className='absolute top-0 right-0 p-2 text-primary hover:text-accent transition-transform transform hover:scale-110'
-          >
-            <FaTimes size={24} />
-          </button>
-          <button
-            onClick={prevPhoto}
-            className='absolute left-0 top-1/2 transform -translate-y-1/2 p-2 text-primary hover:text-accent transition-transform hover:scale-110'
-          >
-            <FaArrowLeft size={24} />
-          </button>
-          <div className='relative w-full h-full overflow-hidden'>
-            <AnimatePresence initial={false} custom={direction}>
-              <motion.div
-                key={activePhotoIndex}
-                custom={direction}
-                variants={variants}
-                initial="enter"
-                animate="center"
-                exit="exit"
-                transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-                className='absolute top-0 left-0 w-full h-full'
-              >
-                <ImageComponent
-                  src={`${process.env.PUBLIC_URL}/gallery-photos/${galleryPhotos[activePhotoIndex].photo}`}
-                  alt={`Photo ${activePhotoIndex + 1}`}
-                  className='object-cover w-full  rounded-sm'
-                  blurHash={galleryPhotos[activePhotoIndex].blurhash}
-                />
-              </motion.div>
-            </AnimatePresence>
+      {isLargeScreen && (
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={closeModal}
+          contentLabel="Photo Modal"
+          className="flex items-center justify-center outline-none"
+          overlayClassName="fixed inset-0 bg-black bg-opacity-50 z-30 flex items-center justify-center"
+        >
+          <div className='relative z-35 h-120 min-w-[34rem] w-full bg-secondary p-10 rounded'>
+            <button
+              onClick={closeModal}
+              className='absolute top-0 right-0 p-2 text-primary hover:text-accent transition-transform transform hover:scale-110'
+            >
+              <FaTimes size={24} />
+            </button>
+            <button
+              onClick={prevPhoto}
+              className='absolute left-0 top-1/2 transform -translate-y-1/2 p-2 text-primary hover:text-accent transition-transform hover:scale-110'
+            >
+              <FaArrowLeft size={24} />
+            </button>
+            <div className='relative w-full h-full overflow-hidden'>
+              <AnimatePresence initial={false} custom={direction}>
+                <motion.div
+                  key={activePhotoIndex}
+                  custom={direction}
+                  variants={variants}
+                  initial="enter"
+                  animate="center"
+                  exit="exit"
+                  transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+                  className='absolute top-0 left-0 w-full h-full'
+                >
+                  <ImageComponent
+                    src={`${process.env.PUBLIC_URL}/gallery-photos/${galleryPhotos[activePhotoIndex].photo}`}
+                    alt={`Photo ${activePhotoIndex + 1}`}
+                    className='object-cover w-full  rounded-sm'
+                    blurHash={galleryPhotos[activePhotoIndex].blurhash}
+                  />
+                </motion.div>
+              </AnimatePresence>
+            </div>
+            <button
+              onClick={nextPhoto}
+              className='absolute right-0 top-1/2 transform -translate-y-1/2 p-2 text-primary hover:text-accent transition-transform hover:scale-110'
+            >
+              <FaArrowRight size={24} />
+            </button>
           </div>
-          <button
-            onClick={nextPhoto}
-            className='absolute right-0 top-1/2 transform -translate-y-1/2 p-2 text-primary hover:text-accent transition-transform hover:scale-110'
-          >
-            <FaArrowRight size={24} />
-          </button>
-        </div>
-      </Modal>
+        </Modal>
+      )}
     </div>
   );
 }
