f5918053711fe5876d31d8b0a34bc57c155bc976
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-07-28 21:35:31 -0600

    updated phone view on hero

--- src/homeSections/hero/HeroText.jsx
+++ src/homeSections/hero/HeroText.jsx
@@ -1,41 +1,6 @@
-/**
- * @file HeroText.jsx
- * @module HeroText
- * @desc React component that displays the text content of the hero section.
- * The text includes a title, subtitle, and buttons, with an option to open a PDF.
- *
- * @component HeroText
- * 
- * @requires react
- * @requires motion, useScroll, useTransform from 'framer-motion'
- * @requires heroData from '../../data/pageData/heroData'
- * @requires MyButton from '../../components/uiElements/MyButton'
- * @requires openPdf from '../../functions/utils'
- * 
- * @see {@link https://reactjs.org/docs/getting-started.html | React Documentation}
- * @see {@link https://www.framer.com/motion/ | Framer Motion Documentation}
- * 
- * @example
- * // Example usage of HeroText component
- * import HeroText from './HeroText';
- * 
- * function App() {
- *   return (
- *     <div className="App">
- *       <HeroText />
- *     </div>
- *   );
- * }
- * 
- * @exports HeroText
- * 
- * @author Chace Nielson
- * @created 2024-07-28
- * @updated 2024-07-28
- */
-
 import React from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
+import { useMediaQuery } from 'react-responsive';
 import { heroData } from '../../data/pageData/heroData';
 import MyButton from '../../components/uiElements/MyButton';
 import { openPdf } from '../../functions/utils';
@@ -53,17 +18,10 @@ const textVariants = {
       staggerChildren: 0.1,
     },
   },
-  scrollButton: {
-    opacity: 0,
-    y: 10,
-    transition: {
-      duration: 2,
-      repeat: Infinity,
-    },
-  },
 };
 
 function HeroText() {
+  const isSmallScreen = useMediaQuery({ query: '(max-width: 768px)' });
   const { scrollY } = useScroll();
   const y = useTransform(scrollY, [0, 500], [0, 140]);
 
@@ -73,7 +31,7 @@ function HeroText() {
       variants={textVariants}
       initial="initial"
       animate="animate"
-      style={{ y, willChange: 'transform' }}
+      style={isSmallScreen ? {} : { y, willChange: 'transform' }}
     >
       <motion.div variants={textVariants}>
         <motion.h3 variants={textVariants} className="pl-2">
